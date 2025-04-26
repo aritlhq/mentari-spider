7de67abd8eb62fcaae87596cb7364ee5f5a3619b
aritlhq <aritlhq@gmail.com>
2025-04-27 03:47:48 +0700

    feat(types): add MeetingDetails interface for attendance management

---
+++ types/attendace.ts
@@ -0,0 +1,13 @@
+export interface MeetingDetails {
+    jenis_perkuliahan: string;
+    id_semester_registrasi: string;
+    id_mata_kuliah: string;
+    id_kelas: string;
+    ket_jam: string;
+    tanggal_mulai: string;
+    tanggal_akhir: string;
+    presensi_status: string;
+    presensi_date: string;
+    presensi_by: string;
+    date_status: string;
+}
