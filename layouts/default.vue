8b443f80e057a777e4ceec839b8a525524fd7c84
aritlhq <aritlhq@gmail.com>
2025-04-27 03:44:47 +0700

    feat(layout): add default layout with navbar and content slot

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+import AppNavbar from '../components/AppNavbar.vue';
+</script>
+
+<template>
+    <div class="min-h-screen px-10 py-8 flex-col">
+        <AppNavbar/>
+        <slot/>
+    </div>
+</template>
