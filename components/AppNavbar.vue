cf26af9771f4a8c38c23fd7d41b1d7fae3458196
aritlhq <aritlhq@gmail.com>
2025-04-27 03:45:59 +0700

    feat(navbar): add AppNavbar component with navigation links

---
+++ components/AppNavbar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import AppAuthModal from './AppAuthModal.vue'
+
+import { ref } from 'vue'
+
+const showModal = ref(false)
+
+const handleBearerSubmit = () => {
+    showModal.value = false
+}
+</script>
+
+<template>
+    <div class="flex gap-20 mb-20">
+        <NuxtLink to="/" class="btn-green"><< Back to Home</NuxtLink>
+        <div class="flex gap-4">
+            <NuxtLink to="/dashboard" class="btn-green">Dashboard</NuxtLink>
+            <NuxtLink to="/attendance" class="btn-green">Attendance</NuxtLink>
+            <NuxtLink to="/mentari-lms" class="btn-green">Mentari LMS</NuxtLink>
+            <!-- <button @click="showModal = true" class="btn-green">Fetch your Data</button> -->
+        </div>
+
+        <!-- <AuthModal v-if="showModal" @close="showModal = false" @submit="handleBearerSubmit"/> -->
+    </div>
+</template>
