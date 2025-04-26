1a26ed552eeabff36ebebb8e8b769d27ecf82938
aritlhq <aritlhq@gmail.com>
2025-04-27 03:46:07 +0700

    feat: add Welcome component with branding and navigation link

---
+++ components/Welcome.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="min-h-screen font-mono flex items-center justify-center">
+        <div>
+            <div class="flex-col flex gap-y-4">
+                <div class="text-center text-3xl font-bold">
+                    Welcome to
+                </div>
+                <pre>
+            ███╗   ███╗███████╗███╗   ██╗████████╗ █████╗ ██████╗ ██╗    ███████╗██████╗ ██╗██████╗ ███████╗██████╗ 
+            ████╗ ████║██╔════╝████╗  ██║╚══██╔══╝██╔══██╗██╔══██╗██║    ██╔════╝██╔══██╗██║██╔══██╗██╔════╝██╔══██╗
+            ██╔████╔██║█████╗  ██╔██╗ ██║   ██║   ███████║██████╔╝██║    ███████╗██████╔╝██║██║  ██║█████╗  ██████╔╝
+            ██║╚██╔╝██║██╔══╝  ██║╚██╗██║   ██║   ██╔══██║██╔══██╗██║    ╚════██║██╔═══╝ ██║██║  ██║██╔══╝  ██╔══██╗
+            ██║ ╚═╝ ██║███████╗██║ ╚████║   ██║   ██║  ██║██║  ██║██║    ███████║██║     ██║██████╔╝███████╗██║  ██║
+            ╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝    ╚══════╝╚═╝     ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝         
+                                                                                                        By: <a href="http://github.com/aritlhq" class="underline">aritlhq</a>
+        </pre>
+            </div>
+            <div class="flex-col flex gap-y-8">
+                <p class="text-red-700 text-center text-lg">is an autonomous exploitation toolkit built to liberate your
+                    time from the
+                    soul-crushing grind of repetitive LMS tasks.</p>
+
+                <div class="text-center">
+                    <NuxtLink to="/dashboard"
+                        class="btn-green px-2 py-2">
+                        >> Go Here << </NuxtLink>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
