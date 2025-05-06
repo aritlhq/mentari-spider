842b0595c772e3b38cd0824bab062985d5c3ef30
aritlhq <aritlhq@gmail.com>
2025-05-06 17:06:47 +0700

    feat(nuxt.config): add proxy rule for quiz API endpoint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,11 +10,14 @@ export default defineNuxtConfig({
   },
   /**
    * Reference: 
-   * https://mookypoo.medium.com/nuxt-js-how-to-handle-cors-error-a4c5022611d0
+   * https://mookypoo.medium.com/nuxt-js-how-to-handle-cors-error-a4c5100611d0
    */
   routeRules: {
     '/api/mentari/**': {
       proxy: { to: 'https://mentari.unpam.ac.id/api/**' }
+    },
+    '/api/quiz/**': {
+      proxy: { to: 'https://mentari.unpam.ac.id/api/quiz/**' }
     }
   }
 })
